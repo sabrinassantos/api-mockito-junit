67cc190c2d960460b9e947c5eb822baca0fc2e10
Sabrina Silva <sabrina.silva@dbserver.com.br>
2023-01-26 22:56:14 -0300

    inicial

---
+++ src/main/java/br/com/apimockito/apimockito/ApiMockitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.apimockito.apimockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMockitoApplication.class, args);
+	}
+
+}
